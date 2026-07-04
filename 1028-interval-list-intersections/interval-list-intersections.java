e31972a5b0b05cdd5f7d2cf02e5d3174f1f924c6
Pratik Deshmukh <pratikdeshmukh12102004@gmail.com>
2026-07-04 19:24:48 +0530

    Time: 3 ms (99.55%) | Memory: 47.6 MB (19.25%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -1,38 +1,37 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-      int i = 0 ;
-      int j= 0 ;
-      List<int[]>list = new ArrayList<>();
+        int i = 0;
+        int j = 0 ;
+        List<int[]>list = new ArrayList<>();
         while(i<firstList.length && j<secondList.length){
-        int s1=firstList[i][0];
-        int e1 =firstList[i][1] ;
-        int s2=secondList[j][0];
-        int e2 =secondList[j][1] ;
-        //part1
-        if(s1<=s2){
-            if(e1>=s2){
-             int s = Math.max(s1,s2);
-             int e =Math.min(e1,e2);
-            list.add(new int[]{s,e});
+         int s1 = firstList[i][0];
+         int e1 = firstList[i][1];
+         
+         int s2 = secondList[j][0];
+         int e2 = secondList[j][1];
+
+         if(s1 <= s2){
+            if(e1 >= s2){
+                int s = Math.max(s1,s2);
+                int e = Math.min(e1,e2);
+                list.add(new int []{s,e}) ;
             }
-            
-        }
-            else{
-            if(e2>= s1){
-             int s = Math.max(s1,s2);
-             int e =Math.min(e1,e2);
-             list.add(new int[]{s,e});
+         }
+         else{
+            if(e2 >= s1){
+                int s = Math.max(s1,s2);
+                int e = Math.min(e1,e2);
+                list.add(new int []{s,e}) ;
+
             }
+         }
+         if(e1 < e2 ){
+            i++ ;
+         }else{
+            j++ ;
+         }
+         
         }
-            // part2
-            if(e1< e2){
-                i++ ;
-            }else{
-                j++;
-            }
-        
-        }
-        return list.toArray(new int[list.size()][]);
-        
+       return  list.toArray(new int [list.size()][]) ;
     }
 }
